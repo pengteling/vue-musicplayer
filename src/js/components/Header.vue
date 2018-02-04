46a13b14f0cd01b01214d2f041eeca7d5311da0e
unknown <pengteling@gmail.com>
2018-02-05 00:59:17 +0800

    start

---
+++ src/js/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="components-header">
+        <router-link to ="/">
+          <img src="./../../assets/images/logo.png" alt="" class="logo"/>
+        </router-link>
+        <h1 class="caption">Vue Music Player</h1>
+
+        <div id="jplayer"></div>
+      </div>
+
+</template>
+<script>
+import './header.scss'
+
+</script>
