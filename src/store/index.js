9f4d286d7f84e0c29ce03781a925b6bff5b338e1
pengteling <pengteling@gmail.com>
2018-02-24 17:34:07 +0800

    434

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import list from './modules/list'
-import player from './modules/player'
-
 Vue.use(Vuex)
-
 const debug = process.env.NODE_ENV !== 'production'
-
 export default new Vuex.Store({
-  namespaced: true,
-  modules: {
-    list,
-    player
+  strict: debug,
+  state: {
+    musicList: [],
+    currentIndex: 0,
+    paused: true,
+    volume: 0.8,
+    currentTime: 0,
+    duration: 0,
+    repeatType: 'cycle'
   },
-  strict: debug
+  getters: {},
+  mutations: {},
+  actions: {}
 })
